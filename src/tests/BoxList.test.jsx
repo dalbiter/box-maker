711905939ff53e03769a2eaf0bafcbda0964196c
danx <rushn24@gmail.com>
2024-07-27 15:24:49 -0400

    refactoring tests

--- src/tests/BoxList.test.jsx
+++ src/tests/BoxList.test.jsx
@@ -2,22 +2,31 @@ import { render, fireEvent } from '@testing-library/react';
 import BoxList from '../BoxList';
 import { describe, expect, it } from 'vitest';
 
+function addBox(boxList, height = "2", width = "2", color = "peachpuff") {
+    const heightInput = boxList.getByLabelText("Height");
+    const widthInput = boxList.getByLabelText("Width");
+    const backgroundInput = boxList.getByLabelText("Background Color");
+    fireEvent.change(backgroundInput, { target: { value: color } });
+    fireEvent.change(widthInput, { target: { value: width } });
+    fireEvent.change(heightInput, { target: { value: height } });
+    const button = boxList.getByText("Add a new box!");
+    fireEvent.click(button);
+  }
+
 describe('BoxList', () => {
     it('renders BoxList component', () => {
-        render(<BoxList />)
+        render(<BoxList />);
     });
 
     it('should match snapshot', () => {
-        const { asFragment } = render(<BoxList />)
-        expect(asFragment()).toMatchSnapshot()
+        const { asFragment } = render(<BoxList />);
+        expect(asFragment()).toMatchSnapshot();
     });
 
     it('should add a box', () => {
         const { getByLabelText, queryByText } = render(<BoxList />)
-        const widthInput = getByLabelText('Width:')
-        const heightInput = getByLabelText('Height:')
-        const colorInput = getByLabelText('Color:')
-        const btn = queryByText('Create Box!')
-        console.log(boxes) 
+        
+
+         
     });
 });
