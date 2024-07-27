3934cac3cd865ea4cc362ff651cdb6d83360b286
danx <rushn24@gmail.com>
2024-07-27 15:56:02 -0400

    completed specialized tests for BoxList

--- src/tests/BoxList.test.jsx
+++ src/tests/BoxList.test.jsx
@@ -2,16 +2,16 @@ import { render, fireEvent } from '@testing-library/react';
 import BoxList from '../BoxList';
 import { describe, expect, it } from 'vitest';
 
-function addBox(boxList, height = "2", width = "2", color = "peachpuff") {
-    const heightInput = boxList.getByLabelText("Height");
-    const widthInput = boxList.getByLabelText("Width");
-    const backgroundInput = boxList.getByLabelText("Background Color");
+function addBox(boxList, height = "100", width = "100", color = "peachpuff") {
+    const heightInput = boxList.getByLabelText("Height:");
+    const widthInput = boxList.getByLabelText("Width:");
+    const backgroundInput = boxList.getByLabelText("Color:");
     fireEvent.change(backgroundInput, { target: { value: color } });
     fireEvent.change(widthInput, { target: { value: width } });
     fireEvent.change(heightInput, { target: { value: height } });
-    const button = boxList.getByText("Add a new box!");
+    const button = boxList.getByText("Create Box!");
     fireEvent.click(button);
-  }
+  };
 
 describe('BoxList', () => {
     it('renders BoxList component', () => {
@@ -24,9 +24,28 @@ describe('BoxList', () => {
     });
 
     it('should add a box', () => {
-        const { getByLabelText, queryByText } = render(<BoxList />)
+        const boxList = render(<BoxList />);
         
+        expect(boxList.queryByText("X")).not.toBeInTheDocument();
 
-         
+        addBox(boxList);
+
+        const removeButton = boxList.getByText('X');
+        expect(removeButton).toBeInTheDocument();
+        // expect there to be a box
+        expect(removeButton.closest("div")).toHaveClass("Box")
+        //form should have 3 empty inputs
+        expect(boxList.getAllByDisplayValue("")).toHaveLength(3);     
     });
+
+    it("should remove a box", function() {
+        const boxList = render(<BoxList />);
+        addBox(boxList);
+      
+        const removeButton = boxList.getByText("X");
+      
+        // click the remove button and the box should be gone
+        fireEvent.click(removeButton);
+        expect(removeButton).not.toBeInTheDocument();
+      });
 });
